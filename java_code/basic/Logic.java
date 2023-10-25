d69d28cb076fa96c49001d08dfc484e2eba82088
mnl005 <mnl005@naver.com>
2023-10-26 01:01:34 +0900

    save

---
+++ java_code/basic/Logic.java
@@ -0,0 +1,49 @@
+package basic;
+
+public class Logic {
+    public static void main(String[] args) {
+        //단항연산자
+        System.out.println(3 + 1);
+        System.out.println(3 - 1);
+        System.out.println(3 * 1);
+        System.out.println(3 % 1);
+
+        //증감연산자
+        int i = 0;
+        System.out.println(i++);
+        System.out.println(++i);
+
+        //논리부정
+        System.out.println(!true);
+
+        //비트반전
+        int a = 1;
+        System.out.println(~a);
+
+        //정확히 계산시 부동소수점 사용하는게 아닌 정수를 사용
+        //NaN은 숫자아님(!= 연산자 제외한 모든 비교 연산자에서 false)
+        //Infinity는 무한
+        //문자열 연결 연산자에서 숫자사용시 사용순서가 값에관여
+
+        //비교연산자
+        System.out.println(1 > 2);
+        System.out.println(1 == 1.0);
+        System.out.println(1 != 1.0);
+        System.out.println(1 >= 1.0);
+
+        //논리 연산자
+        System.out.println(true && false);
+        System.out.println(true || false);
+        System.out.println(true ^ false);
+        System.out.println(!false);
+
+        //대입 연산자
+        int b = 0;
+        System.out.println(b += 1);
+
+        //삼항 연산자
+        boolean bool = (0 > 1) ? true : false;
+        System.out.println(bool);
+
+    }
+}
---
+++ java_code/basic/object.java
@@ -0,0 +1,45 @@
+package basic;
+
+// 자바의 타입은 기본 타입(정수 타입, 실수 타입, 논리 타입) 과 참조 타입(배열,열거,클래스, 인터 페이스)
+// 참조 타입은 번지를 통해 객체를 참조
+// 메소드 영역은 클래스 들을 읽어 이것을 모든 스레드 가 공유 한다
+// 힙 영역은 객체와 배열이 생성 되는 영역 이며 다른 영역 에서 참조 하고 참조 하지 않는 객체는 쓰레기 수집기 가 자동 으로 제거 한다
+// JVM 스택 영역은 메소드 호출시 프레임 을 push,pop 한다
+
+import java.util.Arrays;
+
+public class object {
+
+    public int a = 0;
+    static int b = 1;
+    int c = 2;
+
+
+    public static void main(String[] args){
+
+        object obj1 = new object();
+        System.out.println("a : " + obj1.a);
+        System.out.println("b : " + b);
+        System.out.println("c : " + obj1.c);
+
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+        for(int i : arr1){
+            System.out.println(i);
+        }
+        System.out.println("배열 a의 길이 : " + arr1.length);
+        String[] arr2 = {"a","b","c"};
+        System.out.println("arr2[0] = " + arr2[0]);
+
+
+
+
+
+
+
+
+
+    }
+}
