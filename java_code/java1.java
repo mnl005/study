f1200149d8bd87f9d59c82dfa1e77f866c11e743
mnl005 <mnl005@naver.com>
2024-10-01 15:43:44 +0900

    save

--- java_code/java1.java
+++ java_code/java1.java
@@ -25,6 +25,21 @@
 // 논리 : !, &, |, &&, ||
 // 조건 : (조건식) ? A:B
 
+// 객체의 번지를 참조하는 타입 : 배열, 열거, 클래스, 인터페이스
+// 기본타입은 실제값을 변수에 저장
+// 참조타입은 메모리 번지를 변수에 저장
+
+// JVM이 운영체제로부터 할당받은 메모리 영역은 메소드,힙,JVM스택 영역으로 구분
+// 메소드 영역 : 모든 스레드가 공유, 클래스들을 클래스 로더로 읽어 정적필드,상수,메소드코드,생성자코드로 분류 후 저장
+// 힙 영역 : 객체가 생성되는 영역, 다른 영역에서 해당 영역 내의 데이터 참조, 참조되지 않는 데이터는 자동 제거
+// JVM스택 영역 : 메소드 호출.종료시 프레임을 추가.제거 하는 동작을 반복
+// 기본타입 변수는 JVM스택영역에 직접 생성
+// 객체는 선언 후 초기화시 힙영역에 존재하고, 해당 객체를 가르키는 힙영역 주소가 JVM스택에 존재
+
+// 객체 번지 비교 : ==
+// 객체 값 비교 : equals
+// 객체 = null : 참조 타입 변수가 힙 영역 객체를 참조하지 않는다
+
 
 
 import java.util.Scanner;
@@ -81,6 +96,52 @@ public class java1 {
         int3 += 1;
         String string3 = (int3 == 2)? "a" : "b";
         System.out.println(string3);
+
+        if(false){
+            System.out.println("false");
+        }
+        else if(false){
+            System.out.println("false");
+        }
+        else{
+            System.out.println("true");
+        }
+
+        switch(2){
+            case 1 :
+                System.out.println(1);
+                break;
+            case 2 :
+                System.out.println(2);
+                break;
+            default:
+                System.out.println("err");
+                break;
+        }
+
+        for(int a = 0; a < 3; a++){
+            System.out.println("for : " + a);
+        }
+
+        while(3 > 0){
+            System.out.println("while");
+            if(true){
+                break;
+            }
+        }
+
+        do{
+            System.out.println("do");
+            if(false){
+                continue;
+            }
+        }
+        while (false);
+
+        String string4 = "a";
+        String string6 = new String("a");
+        System.out.println(string4.equals(string6));
+        System.out.println(string4 == string6);
 
 
 
